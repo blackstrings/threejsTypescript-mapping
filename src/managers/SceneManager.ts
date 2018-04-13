5e7910b3b313f4da17e0d907d8e18b475d3320d7
Xai <xlao@menard-inc.com>
2018-04-13 15:58:34 -0500

    scenemanager cleanup

--- src/managers/SceneManager.ts
+++ src/managers/SceneManager.ts
@@ -20,7 +20,7 @@ export class SceneManager {
   private isCustomDrawEnabled: boolean = false;
   private isShapeEditEnabled: boolean = false;
   private newCustomShapePoints: THREE.Vector3[] = [];
-  
+
   private tempGeoMat: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
   private tempShapePointMeshes: THREE.Mesh[] = [];
 
@@ -35,7 +35,7 @@ export class SceneManager {
       // custom draw if enabled
       this.drawCustomShape(mouseClickPosition);
     });
-    
+
     Subscriptions.debugSetupComplete.subscribe((debug: Debug) => {
       this.debug = debug;
     });
@@ -115,13 +115,13 @@ export class SceneManager {
       if (points) {
 
         const shape: Shape2D = new Shape2D(points);
-        
+
         // show shape position locaiton visually
         if (this.debug && this.debug.enabled && this.debug.showShapePosition) {
           const axisHelper: THREE.AxesHelper = new THREE.AxesHelper(10);
           shape.mesh.add(axisHelper);
         }
-        
+
         this.addShape(shape);
 
       } else {
@@ -134,13 +134,6 @@ export class SceneManager {
           { x: 12, y: -12, z: 0 },
           { x: -12, y: -12, z: 0 }
         ];
-        // const jsonPoints = [
-        //   { x: 0, y: 0, z: 0 },
-        //   { x: 0, y: 12, z: 0 },
-        //   { x: 6, y: 18, z: 0 },
-        //   { x: 12, y: 12, z: 0 },
-        //   { x: 12, y: 0, z: 0 },
-        // ];
 
         const points: THREE.Vector3[] = VectorUtils.convertJsonArrayToVec3s(jsonPoints);
         const s: Shape2D = new Shape2D(points);
@@ -169,18 +162,18 @@ export class SceneManager {
       console.warn('failed to add shape, shape is null');
     }
   }
-  
+
   public getShape(shapeId: number): Shape {
     let shape: Shape = null;
     if (this.shapes && this.shapes.length) {
-      
+
       const shapes: Shape[] = this.shapes.filter((shape) => shape.mesh.id === shapeId);
       if (shapes && shapes.length) {
         shape = shapes[0];
       } else {
         console.warn(`failed to get shape of id ${shapeId}`);
       }
-      
+
     } else {
       console.warn('failed to get shape, shapes is null or empty');
     }
@@ -195,13 +188,26 @@ export class SceneManager {
    * @memberof SceneManager
    */
   private drawCustomShape(mouseClickPosition: THREE.Vector3): void {
+
+    // snapping if we want it
+    const snapValue: number = 12;
+    const mouseClickSnapPos: THREE.Vector3 = new THREE.Vector3();
+    const x: number = Math.round(mouseClickPosition.x / snapValue) * snapValue;
+    const y: number = Math.round(mouseClickPosition.y / snapValue) * snapValue;
+    mouseClickSnapPos.set(x, y, 0);
+
     const maxClick: number = 4;
     if (this.isCustomDrawEnabled) {
-      
+
       // collect the clicks until you reach max click
       if (this.newCustomShapePoints && this.newCustomShapePoints.length < maxClick) {
-        this.createTempPointMesh(mouseClickPosition);
-      
+
+        // store so we can later pass into shape2D
+        this.newCustomShapePoints.push(mouseClickSnapPos);
+        
+        // create temp mesh on mouse click location
+        this.createTempPointMesh(mouseClickSnapPos);
+
         if (this.newCustomShapePoints.length >= maxClick) {
           // draw the shape
           this.createShape(this.newCustomShapePoints);
@@ -209,40 +215,29 @@ export class SceneManager {
           this.setCustomDraw(false);  // turn off
           this.cleanTempPointMeshes();
         }
-        
+
       }
     } // custom draw is not enabled, do nothing
   }
-  
-  private createTempPointMesh(mouseClick: THREE.Vector3): void {
+
+  private createTempPointMesh(mouseClickPos: THREE.Vector3): void {
     if (this.tempShapePointMeshes && this.tempGeoMat) {
-      
+
       // size
       const radius: number = 2;
       const geo: THREE.Geometry = new THREE.CircleGeometry(radius, 8);
       const mesh: THREE.Mesh = new THREE.Mesh(geo, this.tempGeoMat);
-      
-      // get rounded number on grid
-      const snapValue: number = 12;
-      const mouseClickSnapPos: THREE.Vector3 = new THREE.Vector3();
-      const x: number = Math.round(mouseClick.x / snapValue) * snapValue;
-      const y: number = Math.round(mouseClick.y / snapValue) * snapValue;
-      mouseClickSnapPos.set(x, y, 0);
-      
-      // store so we can later pass into shape2D
-      this.newCustomShapePoints.push(mouseClickSnapPos);  
-      
+
       // move each temp circle into position of where the clicked occured
-      mesh.position.copy(mouseClickSnapPos);
-      
+      mesh.position.copy(mouseClickPos);
+
       this.tempShapePointMeshes.push(mesh);
       this.activeScene.add(mesh);
     }
   }
-  
+
   private cleanTempPointMeshes(): void {
     if (this.tempShapePointMeshes && this.tempShapePointMeshes.length) {
-      
       this.tempShapePointMeshes.forEach((mesh) => {
         this.activeScene.remove(mesh);
       });
@@ -253,7 +248,7 @@ export class SceneManager {
   public setCustomDraw(value: boolean): void {
     this.isCustomDrawEnabled = value;
   }
-  
+
   public setShapeEdit(value: boolean): void {
     this.isShapeEditEnabled = value;
   }
